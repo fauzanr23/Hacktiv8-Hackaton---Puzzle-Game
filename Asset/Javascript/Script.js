f6b917227a16b40c03d74f733530da0799b5d6c2
fauzanr23 <fauzan.rachmatullah23@gmail.com>
2026-01-29 14:10:21 +0000

    testing random gambar

--- Asset/Javascript/Script.js
+++ Asset/Javascript/Script.js
@@ -41,7 +41,7 @@ function btnInputName() {
     });
     return;
   }
-  window.location.href= "game.html"
+  window.location.href = "game.html"
 }
 
 
@@ -49,11 +49,6 @@ function btnInputName() {
 function startGame() {
   const container = document.getElementById('mainContainer');
   container.classList.add('fade-out');
-
-  setTimeout(() => {
-    // Redirect ke halaman game
-    window.location.href = 'onet-game.html';
-  }, 500);
 }
 
 // Easter egg: tekan spasi untuk start
@@ -63,3 +58,80 @@ document.addEventListener('keydown', (e) => {
     startGame();
   }
 });
+
+
+//Database gambar kartu
+
+let dbGambar = [
+  { id: 1, gambar: "apple", src: "Asset/Images/Kartu/apple.png" },
+  { id: 2, gambar: "apple", src: "Asset/Images/Kartu/apple.png" },
+  { id: 3, gambar: "bananas", src: "Asset/Images/Kartu/bananas.png" },
+  { id: 4, gambar: "bananas", src: "Asset/Images/Kartu/bananas.png" },
+  { id: 5, gambar: "cherries", src: "Asset/Images/Kartu/cherries.png" },
+  { id: 6, gambar: "cherries", src: "Asset/Images/Kartu/cherries.png" },
+  { id: 7, gambar: "dragon fruit", src: "Asset/Images/Kartu/dragon-fruit.png" },
+  { id: 8, gambar: "dragon fruit", src: "Asset/Images/Kartu/dragon-fruit.png" },
+  { id: 9, gambar: "grapes", src: "Asset/Images/Kartu/grapes.png" },
+  { id: 10, gambar: "grapes", src: "Asset/Images/Kartu/grapes.png" },
+  { id: 11, gambar: "orange", src: "Asset/Images/Kartu/orange.png" },
+  { id: 12, gambar: "orange", src: "Asset/Images/Kartu/orange.png" },
+  { id: 13, gambar: "passion fruit", src: "Asset/Images/Kartu/passion-fruit.png" },
+  { id: 14, gambar: "passion fruit", src: "Asset/Images/Kartu/passion-fruit.png" },
+  { id: 15, gambar: "pineapple", src: "Asset/Images/Kartu/pineapple.png" },
+  { id: 16, gambar: "pineapple", src: "Asset/Images/Kartu/pineapple.png" },
+  { id: 17, gambar: "strawberry", src: "Asset/Images/Kartu/strawberry.png" },
+  { id: 18, gambar: "strawberry", src: "Asset/Images/Kartu/strawberry.png" },
+  { id: 19, gambar: "watermelon", src: "Asset/Images/Kartu/watermelon.png" },
+  { id: 20, gambar: "watermelon", src: "Asset/Images/Kartu/watermelon.png" }
+]
+
+
+//function display gambar di kartu
+
+let dbTemp = []
+
+function randomGambar() {
+
+  if (dbTemp.length === dbGambar.length) {
+    return
+  }
+  let random = Math.floor(Math.random() * 20)
+  let object = dbGambar[random]
+
+  let validasi = validasiGambar(object) //
+
+  if (validasi === false) {
+    return object.src
+  } else {
+    return randomGambar()
+  }
+}
+
+
+function validasiGambar(object) {
+
+  for (let i = 0; i < dbTemp.length; i++) {
+    let perObj = dbTemp[i]
+
+    if (object.id === perObj.id) {
+      return true
+    }
+  }
+
+    dbTemp.push(object)
+    return false
+}
+
+let cards = document.getElementsByClassName("cardImg")
+for (let i = 0; i < cards.length; i++) {
+  let perCard = cards[i] //object
+  let Gambar = randomGambar()
+  if (Gambar) {
+    perCard.src = Gambar
+  }
+
+}
+
+
+
+
